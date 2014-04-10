951bf189ca2dfb7a48d0c8633046242e9cafc7b2
eazama <eazama001@gmail.com>
2014-04-09 17:47:12 -0700

    Disabled and Grid scripts
    Disabled script added an inspector element to make an object active or
    inactive.
    Grid generates a grid of unused box colliders to create a visible grid
    in the scene view. Mostly using to check entity placement on the grid.

---
+++ Assets/Scripts/GameSceneScripts/Disabled.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disabled : MonoBehaviour {
+
+	public bool Active = true;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (Active);
+	}
+}
---
+++ Assets/Scripts/GameSceneScripts/Grid.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		BoxCollider col;
+		for(int i = 416; i >= 0; i-=32){
+			col = gameObject.AddComponent<BoxCollider>();
+			col.center = new Vector3(i,0,0);
+			col.size = new Vector3(0,800,0);
+
+			col = gameObject.AddComponent<BoxCollider>();
+			col.center = new Vector3(-i,0,0);
+			col.size = new Vector3(0,800,0);
+
+			col = gameObject.AddComponent<BoxCollider>();
+			col.center = new Vector3(0,i,0);
+			col.size = new Vector3(800,0,0);
+
+			col = gameObject.AddComponent<BoxCollider>();
+			col.center = new Vector3(0,-i,0);
+			col.size = new Vector3(800,0,0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
