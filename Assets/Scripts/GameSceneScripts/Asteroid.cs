61b92866da4a880eed3b19aeb6a05d8ea750c901
x3jasmine <x3_momoko@hotmail.com>
2014-04-11 05:31:17 -0700

    Started making the asteroid/debris obstacles
    Also changed the body sprite

---
+++ Assets/Scripts/GameSceneScripts/Asteroid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+	public Asteroid debris = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
