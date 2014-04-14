9eb4c514bcabf63320ad51bcf8998675ff48134c
cdilday <cdilday@ucsc.edu>
2014-04-14 13:05:32 -0700

    Added Gamecontroller & Asteroid Layout
    - Made a Gamecontroller GameObject that will control spawning waves and
    asteroids.
    - Added a random asteroid spawner that spawns a number of asteroids
    based on density at the beginning of the scene

---
+++ Assets/Scripts/GameSceneScripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject asteroid;
+	public float asteroidDensity; //enter a value from 0-100
+	
+	void Start () {
+		spawnAsteroidLayout ();
+	}
+
+	void Update () {
+	
+	}
+
+	void spawnAsteroidLayout()
+	{
+		for (int row = 0; row < 25; row++)
+		{
+			for (int col = 0; col < 25; col++)
+			{
+				if (Random.Range(0, 100) < asteroidDensity)
+				{
+					Instantiate (asteroid, new Vector3((row * 32) + -384, (col * 32) - 384, 0), Quaternion.identity);
+				}
+			}
+		}
+	}
+
+	void spawnEnemyWave()
+	{
+
+	}
+}
