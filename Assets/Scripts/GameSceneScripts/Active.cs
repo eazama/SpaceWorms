7f8aaeda3a3f37c3018587a3739ce130ec4d4eea
eazama <eazama001@gmail.com>
2014-04-15 19:20:45 -0700

    Disabled renamed to Active

---
+++ Assets/Scripts/GameSceneScripts/Active.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Active : MonoBehaviour {
+
+	public bool isActive = true;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (isActive);
+	}
+}
