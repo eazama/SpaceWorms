4f6615d01c087e1fb3732834bbf771b15d173605
cdilday <seesevn@cox.net>
2014-11-17 14:09:28 -0800

    Mute Fix
    Hello again. I realized when I was putting this up on my portfolio that
    the mute button actually increases volume every time you unmute it. I've
    fixed that here, and I'm not sure why a couple other things were
    changed, but everything runs fine still.

--- Assets/Mute.cs
+++ Assets/Mute.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Mute : MonoBehaviour {
 	public GUIStyle mute;
 	public GUIStyle unmute;
+
+	public float savedVolume;
 	
 	// Use this for initialization
 	void Start () {
-		
+		savedVolume = AudioListener.volume;
 	}
 	
 	// Update is called once per frame
@@ -17,10 +19,11 @@ public class Mute : MonoBehaviour {
 	
 	void OnGUI(){
 		if (AudioListener.volume != 0 && GUI.Button (new Rect(5,750,50,50), "", mute)) {
+			savedVolume = AudioListener.volume;
 			AudioListener.volume = 0;
 		}
 		else if (AudioListener.volume == 0 && GUI.Button (new Rect(5,750,50,50), "", unmute)) {
-			AudioListener.volume = 100;
+			AudioListener.volume = savedVolume;
 		}
 	}
 	void Awake() {
